b39ab6e48d56a7fe1c10dd9483709c6b2f655bae
tabish-siraj <tabish1226@gmail.com>
2025-06-28 01:20:47 +0500

    linknest2

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,37 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookmarkForm from "@/components/bookmark-form";
 import BookmarkCard from "@/components/bookmark-card";
-
-interface Bookmark {
-  id: number;
-  title: string;
-  url: string;
-}
+import { Bookmark } from "../src/types";
+import { loadBookmarks, saveBookmarks } from "@/lib/utils";
 
 export default function Home() {
   const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
+
+  useEffect(() => {
+    const stored = loadBookmarks()
+    setBookmarks(stored)
+  }, [])
+
+  useEffect(() => {
+    saveBookmarks(bookmarks)
+
+  }, [bookmarks])
+
   const addBookmark = (bookmark: Bookmark) => {
-    setBookmarks([...bookmarks, bookmark]);
+    setBookmarks(prev => [...prev, bookmark]);
   }
 
   const deleteBookmark = (id: number) => {
-    setBookmarks(bookmarks.filter(b => b.id !==id));
+    setBookmarks(prev => prev.filter(b => b.id !== id))
   }
   return (
     <main className="min-h-screen p-6 flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-6">LinkNest</h1>
-      <BookmarkForm onAdd={addBookmark}/>
+      <BookmarkForm onAdd={addBookmark} />
       <div className="mt-8 flex flex-col gap-4">
-        {bookmarks.map((bookmark)=>(
-          <BookmarkCard key={bookmark.id} bookmark={bookmark} onDelete={deleteBookmark}/>
+        {bookmarks.map((bookmark) => (
+          <BookmarkCard key={bookmark.id} bookmark={bookmark} onDelete={deleteBookmark} />
         ))}
       </div>
     </main>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
+import { Bookmark } from "@/src/types";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const loadBookmarks = () => {
+  if (typeof window == "undefined") return [];
+  try {
+    const stored = localStorage.getItem('bookmarks')
+    return stored ? JSON.parse(stored) : []
+  } catch (error) {
+    console.error("Failed to load bookmark:", error)
+    return []
+  }
+}
+
+export const saveBookmarks = (bookmarks: Bookmark[]) => {
+  try {
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+  } catch (error) {
+    console.error("Failed to save bookmarks:", error)
+  }
 }
